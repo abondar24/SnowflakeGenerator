1b2bd355530e15f17ad2542bd1b4f0c1f37cbe29
Alex Bondar <abondar1992@gmail.com>
2024-07-10 16:29:25 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+}
+
+group = "org.abondar.experimental"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.jetty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-webjars-jvm")
+    implementation("org.webjars:jquery:3.2.1")
+    implementation("io.github.smiley4:ktor-swagger-ui:2.9.0")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-jackson-jvm")
+    implementation("io.ktor:ktor-server-jetty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/org/abondar/experimental/Application.kt
@@ -0,0 +1,13 @@
+package org.abondar.experimental
+
+import io.ktor.server.application.*
+import org.abondar.experimental.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.jetty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
