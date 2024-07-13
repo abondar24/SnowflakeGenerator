2f9feab331a55bb8df27ad41b6cddfd7a478eac7
Alex Bondar <abondar1992@gmail.com>
2024-07-13 19:24:29 +0200

    jib config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.12"
+    id("com.google.cloud.tools.jib") version "3.1.1"
 }
 
 group = "org.abondar.experimental"
-version = "0.0.1"
+version = "1.0.0"
 
 application {
     mainClass.set("io.ktor.server.jetty.EngineMain")
@@ -31,3 +34,42 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "io.ktor.server.jetty.EngineMain"
+    }
+}
+
+var registry = System.getenv("DOCKER_REGISTRY")
+
+jib {
+    from {
+        image = "eclipse-temurin:17-jre"
+        platforms {
+//            platform {
+//                architecture = "amd64"
+//                os = "linux"
+//            }
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+
+    }
+    to {
+        image = "$registry/snowflakegenerator:$version"
+        auth {
+            username = System.getenv("DOCKER_USERNAME")
+            password = System.getenv("DOCKER_PWD")
+        }
+    }
+
+    container {
+        mainClass = mainClass
+        ports = listOf("8080")
+    }
+}
+
